78b6b23bc231284d0a943edc531e0d8f80fce22f
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2022-10-30 23:01:13 -0600

    add basic components to start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphComponent } from './graph/graph.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { GroupsListComponent } from './groups-list/groups-list.component';
+import { ContentContainerComponent } from './content-container/content-container.component';
 
 @NgModule({
-  declarations: [AppComponent, GraphComponent],
+  declarations: [AppComponent, GraphComponent, GroupsListComponent, ContentContainerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgxGraphModule,
     BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
