0d399bdd92a37d4202cb6a0377c0df5e431414fb
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2022-11-15 14:47:31 -0600

    add github link, style buttons

--- src/app/content-container/graph/graph.module.ts
+++ src/app/content-container/graph/graph.module.ts
@@ -4,9 +4,16 @@ import { GraphComponent } from './graph.component';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
 import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [GraphComponent],
-  imports: [CommonModule, NgxGraphModule, MatRadioModule, FormsModule],
+  imports: [
+    CommonModule,
+    NgxGraphModule,
+    MatRadioModule,
+    FormsModule,
+    MatButtonModule,
+  ],
   exports: [GraphComponent],
 })
 export class GraphWorldCupModule {}
